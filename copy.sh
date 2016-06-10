5ba93acb43eea646be8225db90b137bc137da4f2
brayhoward <brandonrhoward@gmail.com>
2016-06-10 08:18:42 -0500

    Release 0.1.0

--- copy.sh
+++ copy.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-npm install bootstrap3-accessibility-patches
+npm install accessible-bootstrap3
 
 rm -rf vendor
 
 mkdir -p vendor/assets/javascripts/accessible-bootstrap3 vendor/assets/javascripts/jquery
 
-cp node_modules/bootstrap3-accessibility-patches/index.js vendor/assets/javascripts/accessible-bootstrap3
+cp node_modules/accessible-bootstrap3/index.js vendor/assets/javascripts/accessible-bootstrap3
 cp -r node_modules/jquery/dist/ vendor/assets/javascripts/jquery
 
-# copy data from bootstrap3-accessibility-patches package.
-rm README.md bootstrap3-accessibility-patches-package.json && touch README.md bootstrap3-accessibility-patches-package.json
+# copy data from bootstrap3-accessibility-patches package.json and README.md
+rm README.md accessible-bootstrap3-package.json && touch README.md accessible-bootstrap3-package.json
 
-cp node_modules/bootstrap3-accessibility-patches/package.json bootstrap3-accessibility-patches-package.json
+cp node_modules/accessible-bootstrap3/package.json accessible-bootstrap3-package.json
 
 
-cat gem-instructions.md >> README.md
-cat node_modules/bootstrap3-accessibility-patches/README.md >> README.md
+cp gem-instructions.md  README.md
+cat node_modules/accessible-bootstrap3/README.md >> README.md
 
 rm -rf node_modules
 
--- lib/accessible/bootstrap3/rails/version.rb
+++ lib/accessible/bootstrap3/rails/version.rb
@@ -3,7 +3,7 @@ require 'json'
 module Accessible
   module Bootstrap3
     module Rails
-      package_info = JSON.parse(File.read(File.expand_path('../../../../../bootstrap3-accessibility-patches-package.json', __FILE__)))
+      package_info = JSON.parse(File.read(File.expand_path('../../../../../accessible-bootstrap3-package.json', __FILE__)))
       VERSION = package_info["version"]
     end
   end
