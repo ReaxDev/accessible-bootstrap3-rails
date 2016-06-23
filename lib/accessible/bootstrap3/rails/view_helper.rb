33d366e78b35f3d9b0b91a7bab157a92664c6e81
brayhoward <brandonrhoward@gmail.com>
2016-06-23 14:09:02 -0500

    Wrap abbreviation in span with aria-hidden attribute.

--- lib/accessible/bootstrap3/rails/view_helper.rb
+++ lib/accessible/bootstrap3/rails/view_helper.rb
@@ -4,7 +4,7 @@ module Accessible
       module ViewHelpers
         def wrap_abbreviations(string)
           string.enum_for(:scan, /([A-Z]{2,})/).reduce(string) do |acc, _|
-            acc.gsub $1, "<abbr title='#{$1.chars.to_a.join(' ')}'>#{$1}</abbr>"
+            acc.gsub $1, "<abbr title='#{$1.chars.to_a.join(' ')}'><span aria-hidden='true'>#{$1}</span></abbr>"
           end.html_safe
         end
 
